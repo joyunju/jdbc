e5885d93371b4f2edf417c8d283740c016258d20
joyunju <whdbswn1113@naver.com>
2022-05-24 00:09:02 +0900

    ResultSet rs = pstmt.executeQuery(); //select

--- src/com/javaex/ex01/BookSelect.java
+++ src/com/javaex/ex01/BookSelect.java
@@ -17,20 +17,43 @@ public class BookSelect {
 		try {
 			// 1. JDBC 드라이버 (Oracle) 로딩
 			Class.forName("oracle.jdbc.driver.OracleDriver");
-			
-			
+
 			// 2. Connection 얻어오기
 			String url = "jdbc:oracle:thin:@localhost:1521:xe";
 			conn = DriverManager.getConnection(url, "webdb", "webdb");
-			
+
 			// 3. SQL문 준비 / 바인딩 / 실행
 			// 3-1. SQL문 준비
-			
+			String query = "";
+			query += " select book_id,";
+			query += "		title,";
+			query += "		pubs,";
+			query += "		to_char(pub_date, 'YYYY-MM-DD'),";
+			query += "		author_id";
+			query += " from book";
+			System.out.println(query);
+
 			// 3-2. 바인딩
-									
-			//3-3. 실행
+			pstmt = conn.prepareStatement(query);
+			
+			// 3-3. 실행
+			rs = pstmt.executeQuery();
 			
 			// 4.결과처리
+			while (rs.next()) {
+				// int bookId = rs.getInt("book_id");
+				// String bookName = rs.getString("book_name");
+				// String bookDesc = rs.getString("book_desc");
+
+				int bookId = rs.getInt(1);
+				String title = rs.getString(2);
+				String pubs = rs.getString(3);
+				String pubDate = rs.getString(4);
+				int authorId = rs.getInt(5);
+
+				System.out.println(bookId + ", " + title + ", " + pubs + ", " + pubDate + ", " + authorId);
+			}
+			
 		} catch (ClassNotFoundException e) {
 			System.out.println("error: 드라이버 로딩 실패 - " + e);
 		} catch (SQLException e) {
